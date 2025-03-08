9dd82ee3e5647bb3fd8eb21e49f22997593d256e
Jeremy Watt <jermwatt@gmail.com>
2025-03-08 11:29:34 -0700

    working on user signup flow to create profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :sessions, dependent: :destroy
+  has_one :user_profile, dependent: :destroy
 
   normalizes :email_address, with: ->(e) { e.strip.downcase }
+  after_create :create_profile
+
+  private
+
+  def create_profile
+    create_user_profile(email_address: email) # Auto-fill email from user
+  end
 end
